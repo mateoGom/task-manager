3d9501d2f696cc4db65f16807e12df696f64e99f
mateoGom <matemgm@hotmail.com>
2020-11-11 21:17:03 +0100

    connecting and inserting documents

---
+++ mongodb.js
@@ -0,0 +1,19 @@
+const mongodb=require('mongodb')
+const MongoClient=mongodb.MongoClient
+
+const connectionURL='mongodb://127.0.0.1:27017'
+const databaseName='task-manager'
+
+MongoClient.connect(connectionURL,{useNewUrlParser:true},(error,client)=>{
+    if (error){
+       return console.log('unable to connect to the server')
+    }
+
+    const db=client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name:'mat',
+        age:23
+    })
+
+})
