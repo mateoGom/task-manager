56182afc6a511c2acb79ba01c90566a2d318d981
mateoGom <matemgm@hotmail.com>
2020-11-12 19:53:13 +0100

    querying documents

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,4 @@
-const mongodb=require('mongodb')
-const MongoClient=mongodb.MongoClient
+const{MongoClient,ObjectId}=require('mongodb')
 
 const connectionURL='mongodb://127.0.0.1:27017'
 const databaseName='task-manager'
@@ -16,19 +15,36 @@ MongoClient.connect(connectionURL,{useNewUrlParser:true},(error,client)=>{
     //     age:23
     // })
 
-    db.collection('users').insertMany([
-        {
-            name:'juana',
-            age:56
-        },{
-            name:'pablo',
-            age:34
-        }
-    ],(error,result)=>{
-        if(error){
-            return console.log('unable to insert the user')
-        }
-        console.log(result.ops)
-    })
+    // db.collection('users').insertMany([
+    //     {
+    //         name:'juana',
+    //         age:56
+    //     },{
+    //         name:'pablo',
+    //         age:34
+    //     }
+    // ],(error,result)=>{
+    //     if(error){
+    //         return console.log('unable to insert the user')
+    //     }
+    //     console.log(result.ops)
+    // })
 
+//     db.collection('users').findOne({ _id: new ObjectId("5fac4584fb622e4e944c323e")}, (error,user)=>{
+//         if(error){
+//             console.log('unable to find user')
+//         }else{
+//             console.log(user)
+//         }
+//     })
+
+
+db.collection('users').find({age:56}).toArray((error,user)=>{
+    if(error){
+        console.log('unable')
+    }else{
+        console.log(user)
+    }
 })
+
+ })
