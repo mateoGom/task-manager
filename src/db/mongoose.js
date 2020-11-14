182ec0ef6eee276e652682b20ddaae546df8fa60
mateoGom <matemgm@hotmail.com>
2020-11-14 16:34:57 +0100

    data validation and sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,28 +1,48 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
+const validator = require("validator");
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
 
-    useNewUrlParser:true,
-    useCreateIndex:true
+const User = mongoose.model("User", {
+  name:{
+      type:String,
+      required:true,
+      trim:true
+  },
+  age:{
+      type: Number,
+      default:0,
+      validate(value){
+          if(value<0){
+              throw new Error('age mnust be a positive number');
+          }
+      }
+  },
+  email:{
+      type: String,
+      required:true,
+      trim:true,
+      lowercase:true,
+      validate(value){
+          if(!validator.isEmail(value)){
+              throw new Error('email is not valid');
+          }
+      }
+  }
+});
 
-})
+const me = new User({
+  name: "helen",
+  email: "asd@ef.com  "
+});
 
-const User=mongoose.model('User',{
-    name:{
-        type: String
-    },
-    age:{
-        type: Number
-    }
-})
-
-const me=new User({
-    name:'Andrew',
-    age:27
-})
-
-me.save().then(()=>{
-    console.log(me)
-}).catch((error)=>{
-    console.log("Error ",error)
-})
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((error) => {
+    console.log("Error ", error);
+  });
