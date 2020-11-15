b295a168cbc9924991c0983640389409f82ed1be
mateoGom <matemgm@hotmail.com>
2020-11-15 16:50:57 +0100

    reading endpoints

--- src/index.js
+++ src/index.js
@@ -1,27 +1,52 @@
-require('./db/mongoose')
-const User=require('./models/users')
+require("./db/mongoose");
+const User = require("./models/users");
 
-const express=require('express')
+const express = require("express");
 
-const app=express()
+const app = express();
 
-const port=process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.post('/users',(req,res)=>{
-    const user=new User(req.body)
+app.post("/users", (req, res) => {
+  const user = new User(req.body);
 
-    user.save().then(()=>{
-        res.send(user)
-    }).catch((e)=>{
-        res.send(e).status(400)
+  user
+    .save()
+    .then(() => {
+      res.send(user);
     })
+    .catch((e) => {
+      res.send(e).status(400);
+    });
+});
 
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users).status(200);
+    })
+    .catch((error) => {
+      res.status(500);
+    });
+});
 
+app.get('/users/:id',(req,res)=>{
+    const _id=req.params.id
 
+    User.findById(_id).then((user)=>{
+        if(!user){
+            return res.status(404)
+        }
+
+        res.send(user)
+
+    }).catch(()=>{
+        res.status(500)
+    })
 })
 
-app.listen(port,()=>{
-    console.log('server is running at port '+port)
-})
+app.listen(port, () => {
+  console.log("server is running at port " + port);
+});
