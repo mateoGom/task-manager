28e12e6529bf6259785d608495a146ae0fed2908
mateoGom <matemgm@hotmail.com>
2020-11-17 17:23:37 +0100

    async/await

--- src/index.js
+++ src/index.js
@@ -9,43 +9,68 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
 
-  user
-    .save()
-    .then(() => {
-      res.send(user);
-    })
-    .catch((e) => {
-      res.send(e).status(400);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error)
+  }
+
+  // user
+  //   .save()
+  //   .then(() => {
+  //     res.send(user);
+  //   })
+  //   .catch((e) => {
+  //     res.send(e).status(400);
+  //   });
 });
 
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send(users).status(200);
-    })
-    .catch((error) => {
-      res.status(500);
-    });
+app.get("/users", async (req, res) => {
+ 
+  const user = await User.find({})
+  try {
+    res.status(200).send(user)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+ 
+  // User.find({})
+  //   .then((users) => {
+  //     res.send(users).status(200);
+  //   })
+  //   .catch((error) => {
+  //     res.status(500);
+  //   });
 });
 
-app.get('/users/:id',(req,res)=>{
-    const _id=req.params.id
+app.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user=await User.findById(_id)
+    if(!user){
+      return res.status(404)
+    }
+    res.status(200).send(user)
+  } catch (error) {
+    res.status(500).send(error)
+  }
 
-    User.findById(_id).then((user)=>{
-        if(!user){
-            return res.status(404)
-        }
+  // User.findById(_id)
+  //   .then((user) => {
+  //     if (!user) {
+  //       return res.status(404);
+  //     }
 
-        res.send(user)
-
-    }).catch(()=>{
-        res.status(500)
-    })
-})
+  //     res.send(user);
+  //   })
+  //   .catch(() => {
+  //     res.status(500);
+  //   });
+});
 
 app.listen(port, () => {
   console.log("server is running at port " + port);
